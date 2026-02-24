d404df8f85e5e4bf5242d043b4974b807235075a
Suraj-S23 <ssurajj08@gmail.com>
2026-02-24 14:41:03 +0100

    redis config

--- backend/utils/session.py
+++ backend/utils/session.py
@@ -11,8 +11,8 @@ import uuid
 import os
 from typing import Optional
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_HOST = os.getenv("REDISHOST", os.getenv("REDIS_HOST", "localhost"))
+REDIS_PORT = int(os.getenv("REDISPORT", os.getenv("REDIS_PORT", "6379")))
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
 
